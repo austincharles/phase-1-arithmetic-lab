fceb379082bdf80321986fd1ab5224c635d75bb4
Austin Holeman <austinholeman@gmail.com>
2022-03-01 10:26:22 -0800

    commit tree aaddfc123074991c332f5189ba199010827e5684

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 7
+Math.floor(Math.random() > 0);
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = 20
+Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
